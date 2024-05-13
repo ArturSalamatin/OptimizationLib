d08f2f35c835c085e282598d831eb7f23a6b6617
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2024-05-13 22:06:39 +0300

    macro for test addition

--- test/foo_test.cpp
+++ test/foo_test.cpp
@@ -3,9 +3,3 @@
 TEST(Foo, Bar) {
   ASSERT_EQ(0, 0);
 }
-
-// int main(int argc, char** argv)
-// {
-//     ::testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
--- test/point_test.cpp
+++ test/point_test.cpp
@@ -43,9 +43,3 @@ TEST(Foo, point) {
 
     ASSERT_EQ(0, 0);
 }
-
-// int main(int argc, char** argv)
-// {
-//     ::testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
